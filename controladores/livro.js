5a72892f961c4dbdc39e49ad826230a303dfcdd1
EliasGsN <gabrielsilva20012005@hotmail.com>
2024-05-07 11:54:47 -0300

    Add serviços

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,7 +1,8 @@
-const fs = require('fs');
+const { getTodosLivros } = require('../servicos/livros');
+
 function getLivros(req, res) {
     try {
-        const livros = JSON.parse(fs.readFileSync("livros.json"));
+        const livros = getTodosLivros();
         res.send(livros)
     } catch (error) {
         res.status(500)
