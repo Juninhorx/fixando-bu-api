08b008b848fb2ff66c86593179681d063b0d4874
EliasGsN <gabrielsilva20012005@hotmail.com>
2024-05-08 15:20:05 -0300

    Adicionadno função postLivro

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const rotaLivro = require('./rotas/livro');
 
 const app = express();
+app.use(express.json());
 
-app.use('/livro', rotaLivro);
+app.use('/livros', rotaLivro);
 
 const port = 8000;
 
